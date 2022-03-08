e510ba631a4965b15d5ff54d412952251b1b0e02
PipeGom <100101763+PipeGom@users.noreply.github.com>
2022-03-08 14:05:16 -0500

    Add files via upload

--- Colegio.cs
+++ Colegio.cs
@@ -8,5 +8,34 @@ namespace SistemaGestionEstudiantil
 {
     internal class Colegio
     {
+
+        public string id;
+        public string name;
+        public string fechaDeNacimiento;
+        public void menu()
+        {
+            Console.Write("----------Bienbenido al sistema de informacion de la institucion educativa Santa Pepita----------");
+            Console.WriteLine("");
+        
+        }
+
+        public void guardarDatos(string id,string name, string fechaDeNacimiento)
+        {
+               
+            Console.
+            this.id = Console.ReadLine();
+            this.name = name;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+
+        }
+
+
+        public void consultarDatos(string id, string name, string fechaDeNacimiento) 
+        { 
+            
+        
+        }
+
+        
     }
 }
