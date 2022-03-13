02f5d747e707ae38a62d6f2087fa9fdb3057691d
PipeGom <100101763+PipeGom@users.noreply.github.com>
2022-03-12 19:39:37 -0500

    Add files via upload

--- Estudiante.cs
+++ Estudiante.cs
@@ -8,5 +8,43 @@ namespace SistemaGestionEstudiantil
 {
     internal class Estudiante
     {
-    }
-}
+        public string id;
+        public string sexo;
+        public string nombre;
+        public string gradoAcursar;
+        public string promedio;
+        public string direccion;
+
+        public Estudiante(string id = "", string sexo = "", string nombre = "", string gradoAcursar = "",
+                                               string promedio = "", string direccion = "")
+        {
+
+            this.id = id;
+            this.sexo = sexo;
+            this.nombre = nombre;
+            this.gradoAcursar = gradoAcursar;
+            this.promedio = promedio;
+            this.direccion = direccion;
+
+        }
+
+        public void matricularUnEstudiante()
+        {
+            Console.WriteLine("Ingrese el id del estudiante");
+            id = Console.ReadLine();
+            Console.WriteLine("Ingrese el sexo del estudiante");
+            sexo = Console.ReadLine();
+            Console.WriteLine("Ingrese el nombre del estudiante");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el grado a cursar del estudiante");
+            gradoAcursar = Console.ReadLine();
+            Console.WriteLine("Ingrese el promedio del estudiante");
+            promedio = Console.ReadLine();
+            Console.WriteLine("Ingrese la direccion de residencia del estudiante");
+            direccion = Console.ReadLine();
+
+
+
+        }
+
+}    }
--- Program.cs
+++ Program.cs
@@ -11,6 +11,71 @@ namespace SistemaGestionEstudiantil
         static void Main(string[] args)
         {
 
+            int Op;
+            string id;
+            string sexo;
+            string nombre;
+            string gradoAcursar;
+            string promedio;
+            string direccion;
+
+            List<Estudiante> ListEstudiantes = new List<Estudiante>();
+            Estudiante E = new Estudiante();
+
+
+            do
+            {
+                Console.WriteLine("Bienvenido al sistema de Gestion de informacion de La institucion educativa santa Pepita\n");
+                Console.WriteLine("1. Matricular un estudiante\n");
+                Console.WriteLine("2. Gestionar la informacion de un estudiante\n");
+                Console.WriteLine("Ingrese el numero de la opcion:\n");
+                Op = int.Parse(Console.ReadLine());
+
+                if (Op == 1)
+                {
+                    do
+                    {
+                        E.matricularUnEstudiante();
+                        id = E.id;
+                        sexo = E.sexo;
+                        nombre = E.nombre;
+                        gradoAcursar = E.gradoAcursar;
+                        promedio = E.promedio;
+                        direccion = E.direccion;
+                        // Se crea la lista de objetos para tener los estudiantes de manera dinamica
+                        ListEstudiantes.Add(new Estudiante(id, sexo, nombre, gradoAcursar, promedio, direccion));
+
+                        Console.WriteLine("Si desea matricular otro estudiante ingrese 1 de lo contrario ingrese 2:");
+                        Op = int.Parse(Console.ReadLine());
+
+                    } while (Op == 1);
+
+                    Console.WriteLine("Los Estudiantes matriculados son");
+
+                    //Para buscar un atributo de un estudiante dentro de la lista esto
+                    //servira para acceder a los atributos y posiblemente modificarlos para la gestion. 
+                    foreach (Estudiante e in ListEstudiantes)
+                    {
+                        Console.WriteLine("El id del estudiante es: " + e.id + " sexo: " + e.sexo + " nombre: " +
+                            e.nombre + " grado a cursar " + e.gradoAcursar + " Promedio " + e.promedio + " direccion " + e.direccion);
+                    }
+                }
+                if (Op == 2)
+                {
+                    Console.WriteLine("Ingrese el Id del estudiante:");
+
+
+                }
+
+                Console.WriteLine("Desea conocer el id del estudiante matriculado?\n");
+
+
+
+                Console.ReadKey();
+
+            }
+            while (Op == 0);
+
         }
     }
 }
