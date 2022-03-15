2d3a995bfd71b182687051482c0e0c006319714c
JhonOlivares26 <89679333+JhonOlivares26@users.noreply.github.com>
2022-03-14 20:12:35 -0500

    Add files via upload

--- Estudiante.cs
+++ Estudiante.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SistemaGestionEstudiantil
 {
-    internal class Estudiante: Acudiente
+    internal class Estudiante : Acudiente
     {
         public string id;
         public string sexo;
@@ -21,7 +21,6 @@ namespace SistemaGestionEstudiantil
         public Estudiante(string id = "", string sexo = "", string nombre = "", string gradoAcursar = "",
                                                string promedio = "", string direccion = "", object acudiente = null)
         {
-
             this.id = id;
             this.sexo = sexo;
             this.nombre = nombre;
@@ -29,83 +28,137 @@ namespace SistemaGestionEstudiantil
             this.promedio = promedio;
             this.direccion = direccion;
             this.acudiente = acudiente;
-
         }
+
 
         public void matricularUnEstudiante(object Acudiente)
         {
-            Console.WriteLine("Ingrese el id del estudiante: ");
+            Console.Write("           Ingrese el id del estudiante: ");
             id = Console.ReadLine();
-            Console.WriteLine("Ingrese el sexo del estudiante: ");
+            Console.Write("           Ingrese el sexo del estudiante: ");
             sexo = Console.ReadLine();
-            Console.WriteLine("Ingrese el nombre del estudiante: ");
+            Console.Write("           Ingrese el nombre del estudiante: ");
             nombre = Console.ReadLine();
-            Console.WriteLine("Ingrese el grado a cursar del estudiante: ");
+            Console.Write("           Ingrese el grado a cursar del estudiante: ");
             gradoAcursar = Console.ReadLine();
-            Console.WriteLine("Ingrese el promedio del estudiante: ");
+            Console.Write("           Ingrese el promedio del estudiante: ");
             promedio = Console.ReadLine();
-            Console.WriteLine("Ingrese la direccion de residencia del estudiante: ");
+            Console.Write("           Ingrese la direccion de residencia del estudiante: ");
             direccion = Console.ReadLine();
             acudiente = Acudiente;
-
         }
 
-        public void modificarDatos(List<Estudiante> b)
-        {
-            /*Console.WriteLine("--------------------Opciones-------------------------");
-            Console.WriteLine("1. Modificar ID");
-            Console.WriteLine("2. Modificar Sexo");
-            Console.WriteLine("3. Modificar Nombre");
-            Console.WriteLine("4. Modificar Grado");
-            Console.WriteLine("5. Modificar Promedio");
-            Console.WriteLine("6. Modificar Direccion");
-            Console.WriteLine("7. Modificar Seguro social");
-            Console.WriteLine("8. Modificar Fecha de Nacimiento");
-            Console.WriteLine("9. Modificar Sede");
-            Console.WriteLine("10. Modificar Condiciones especiales");*/
 
-            Console.WriteLine("Ingrese el Id del estudiante que necesita modificar");
-            string p= Console.ReadLine();
-            foreach (Estudiante e in b)
+        public void modificarDatosEstudiante(List<Estudiante> a)
+        {
+
+            string p = pasarId();
+
+            foreach (Estudiante e in a)
             {
-                if (p == e.id) 
+                if (p == e.id)
                 {
-                    Console.WriteLine("1. Modificar el Id ");
-                    Console.WriteLine("2. Modificar el nombre ");
-                    Console.WriteLine("Ingrese la opcion del dato que desea modificar: ");
+                    Console.WriteLine("                                       -----------------------------Opciones--------------------------------");
+                    Console.WriteLine("                                       1. Modificar ID");
+                    Console.WriteLine("                                       2. Modificar Sexo");
+                    Console.WriteLine("                                       3. Modificar Nombre");
+                    Console.WriteLine("                                       4. Modificar Grado");
+                    Console.WriteLine("                                       5. Modificar Promedio");
+                    Console.WriteLine("                                       6. Modificar Direccion");
+                    Console.Write("                                                 Ingrese la opcion del dato que desea modificar: ");
+
                     int m = int.Parse(Console.ReadLine());
+                    Console.WriteLine("");
+
                     if (m == 1)
                     {
-                        Console.WriteLine("Ingrese el nuevo id:\n");
+                        Console.Write("                                                 Ingrese el nuevo id: ");
                         e.id = Console.ReadLine();
                     }
-                    else if (m == 2) 
+                    else if (m == 2)
                     {
-                        Console.WriteLine("Ingrese el nuevo nombre :\n");
-                        e.nombre = Console.ReadLine();  
+                        Console.Write("                                                 Ingrese el nuevo Sexo : ");
+                        e.sexo = Console.ReadLine();
                     }
+                    else if (m == 3)
+                    {
+                        Console.Write("                                                 Ingrese el nuevo nombre : ");
+                        e.nombre = Console.ReadLine();
+
+                    }
+                    else if (m == 4)
+                    {
+                        Console.Write("                                                 Ingrese el nuevo Grado : ");
+                        e.gradoAcursar = Console.ReadLine();
+
+                    }
+                    else if (m == 5)
+                    {
+                        Console.Write("                                                 Ingrese el nuevo Promedio : ");
+                        e.promedio = Console.ReadLine();
+
+                    }
+                    else if (m == 6)
+                    {
+                        Console.Write("                                                 Ingrese el nuevo Direccion :");
+                        e.direccion = Console.ReadLine();
+
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("                                                 Modificación realizada con éxito");
                 }
-            }
-
-
-        }
-        public string SolicitarInfo(List<Estudiante> b) 
-        {
-            Console.WriteLine("Ingrese el Id del estudiante que necesita Consultar");
-            string j = Console.ReadLine();
-   
-            //Para buscar un atributo de un estudiante dentro de la lista 
-            foreach (Estudiante e in b)
-            {
-                if (j == e.id)
+                else
                 {
-                    Console.WriteLine("El id del estudiante es: \n" + e.id + " sexo: \n" + e.sexo + " nombre: \n" +
-                        e.nombre + " grado a cursar \n" + e.gradoAcursar + " Promedio \n" + e.promedio + " direccion \n" + e.direccion);
+                    Console.WriteLine("No se encontró el Estudiante :(");
                 }
+                    
             }
-            return "El estudiante no se encuentra matriculado :( ";
         }
+                    
+        public string solicitarInfoEstudiante(List<Estudiante> a)
+        {
+            string c = pasarId();
 
-       
+            foreach (Estudiante e in a)
+            {
+                
+                if (e.id == c)
+                {
+                    Console.WriteLine("\nLa información del estudiante con el ID " + e.id + " es: \nnombre: " +
+                            e.nombre + " \nsexo: " + e.sexo + "\ngrado a cursar: " + e.gradoAcursar + "\nPromedio: " + e.promedio + "\ndireccion: " + e.direccion);
+                    decoracion();
+                }
+
+            }
+            return "El estudiante no se encuentra matriculado :(";
+        }
+        public string pasarId()
+        {
+            Console.Write("                                ingrese el ID del estudiante: ");
+            string b = Console.ReadLine();
+            Console.WriteLine("");
+            return b;
+
+        }
+        public void decoracion()
+        {
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+        }
+        public object buscarEstudiante(List<Estudiante> a)
+        {   
+            string c = pasarId();
+            Estudiante N = new Estudiante();
+
+            foreach (Estudiante e in a)
+            {
+                if (e.id == c)
+                {
+                    return e;
+                }
+            }return N;
+        }
+            
+                
+            
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace SistemaGestionEstudiantil
         {
 
             int Op;
+            int Ap;
             List<Estudiante> ListEstudiantes = new List<Estudiante>();
             Estudiante E = new Estudiante();
 
@@ -21,78 +22,98 @@ namespace SistemaGestionEstudiantil
 
             do
             {
-                Console.WriteLine("Bienvenido al sistema de Gestion de informacion de La institucion educativa santa Pepita\n");
-                Console.WriteLine("1. Matricular un estudiante\n");
-                Console.WriteLine("2. Gestionar la informacion de un estudiante\n");
-                Console.WriteLine("Ingrese el numero de la opcion:\n");
+                Console.WriteLine("-----------------Bienvenido al sistema de Gestion de informacion de La institucion educativa santa Pepita--------------\n");
+                Console.WriteLine("1. Matricular un estudiante");
+                Console.WriteLine("2. Gestionar la informacion de un estudiante");
+                Console.WriteLine("3. Gestionar la informacion de un Acudiente\n");
+                Console.Write("Ingrese el numero de la opcion: ");
                 Op = int.Parse(Console.ReadLine());
 
-                while (Op <= 0 || Op >= 3)
+
+                while (Op <= 0 || Op >= 4)
                 {
-                    Console.WriteLine("Solo puede escoger la opcion 1 o 2:");
+                    Console.WriteLine("Solo puede escoger la opcion 1, 2 o 3:");
                     Op = int.Parse(Console.ReadLine());
                 }
+
                 if (Op == 1)
 
                 {
                     do
                     {
-                        Console.WriteLine("----------------------------------------------------------------------------------------------------------");
-                        A.AgregarAcudiente();
+                        E.decoracion(); 
+                        A.agregarAcudiente();
                         ListAcudiente.Add(new Acudiente(A.id, A.sexo, A.nombre, A.contacto, A.parentezco));
-                        E.matricularUnEstudiante(A.PasarAcudiente(ListAcudiente));
-
-                        //A.AgregarAcudiente();
+                        E.matricularUnEstudiante(A.pasarAcudiente(ListAcudiente));
+                        Console.WriteLine("\n                     Estudiante registrado con éxito \n");
 
                         // Se crea la lista de objetos para tener los estudiantes de manera dinamica
                         ListEstudiantes.Add(new Estudiante(E.id, E.sexo, E.nombre, E.gradoAcursar, E.promedio, E.direccion, E.acudiente));
 
-                        Console.WriteLine("1. Desea ingresar un nuevo estudiante ");
-                        Console.WriteLine("2. Desea gestionar la informacion de un estudiante");
+                        Console.WriteLine("                     -----------------------------Opciones--------------------------------");
+                        Console.WriteLine("                     1.Desea ingresar un nuevo estudiante ");
+                        Console.WriteLine("                     2.Desea gestionar la informacion de un estudiante ");
+                        Console.WriteLine("                     3.Desea gestionar la informacion de un Acudiente \n");
+                        Console.Write("                     Ingrese el numero de la opcion que desea realizar: ");
                         Op = int.Parse(Console.ReadLine());
+                        Console.WriteLine("");
 
                     } while (Op == 1);
 
-                    Console.WriteLine("Los Estudiantes matriculados son");
+                    Console.WriteLine("Los Estudiantes matriculados son: ");
 
                     //Para buscar un atributo de un estudiante dentro de la lista esto
                     //servira para acceder a los atributos y posiblemente modificarlos para la gestion. 
                     foreach (Estudiante e in ListEstudiantes)
                     {
                         Console.WriteLine("\nEl id del estudiante es: " + e.id + "\nsexo: " + e.sexo + "\nnombre: " +
-                            e.nombre + "\ngrado a cursar: " + e.gradoAcursar + "\nPromedio: " + e.promedio + "\ndireccion: " +
-                            e.direccion + "\nacudiente: " + e.acudiente);
-                        
+                            e.nombre + "\ngrado a cursar: " + e.gradoAcursar + "\nPromedio: " + e.promedio + "\ndireccion: " + e.direccion + "\nacudiente: " + e.acudiente);
                     }
                 }
-
-                
-                if (Op == 2)
+                if(Op == 2)
 
                 {
-                    Console.WriteLine("------------------------------------------------------------------------------------------------------------");
-                    Console.WriteLine("1. Modificar informacion de un estudiante.");
-                    Console.WriteLine("2. Solicitar info de un estudiante. ");
-                    int p = int.Parse(Console.ReadLine());
-                    if (p == 1)
-                    {
-                        E.modificarDatos(ListEstudiantes);
-                    }
-                    if (p == 2)
+                    Console.WriteLine("-----------------------------Opciones--------------------------------");
+                    Console.WriteLine("           1. Modificar informacion del estudiante");
+                    Console.WriteLine("           2. Solicitar informacion del estudiante\n");
+                    Console.Write("                     Ingrese el numero de la opcion: ");
+                    Ap = int.Parse(Console.ReadLine());
+                    Console.WriteLine("");
 
+                    if (Ap == 1)
                     {
-                        E.SolicitarInfo(ListEstudiantes);
+
+                        E.modificarDatosEstudiante(ListEstudiantes);
                     }
+                    else if (Ap == 2)
+                    {
+                        E.solicitarInfoEstudiante(ListEstudiantes);
+                    }
+                   
+                }
+                if (Op == 3)
+
+                {
+                    Console.WriteLine("-----------------------------Opciones--------------------------------");
+                    Console.WriteLine("            1. Modificar informacion del Acudiente");
+                    Console.WriteLine("            2. Solicitar informacion del Acudiente\n");
+                    Console.Write("                      Ingrese el numero de la opcion: ");
+                    Ap = int.Parse(Console.ReadLine());
+                    if (Ap == 1)
+                    {
+                        A.modificarDatosAcudiente(ListAcudiente);
+                    }
+                    else if (Ap == 2)
+                    {
+                        A.solicitarInfoAcudiente(ListAcudiente);
+                    }
+
                 }
 
-                Console.WriteLine("Se verifica la info");
-                E.SolicitarInfo(ListEstudiantes);
-                Console.ReadKey();
+
             }
-            while (Op == 0);
-            
+            while (true);
+            Console.ReadKey();
         }
-
-
     }
 }
